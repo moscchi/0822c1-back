b3ac34270ca0ad077341bb54fd31c8e8506bfa84
Moschi <smoschini@kovix-group.com>
2023-02-17 21:09:45 -0300

    Segunda clase con metodos HTTP y express

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
-const http =require('http');
-const fs = require('fs/promises')
-const server = http.createServer(async (request, response) => {
-    if(request.url === '/sobreescribir'){
-        await fs.writeFile('./folder/ejemplo.txt', 'archivo sobreescrito.')
-        response.write(`Yo que vos, me voy a revisar el archivo :O`)
-        response.end();
-    }
-    if(request.url === '/agregar'){
-        await fs.appendFile('./folder/ejemplo.txt', 'Agregando contenido al archivo ');
-        response.write('Agregado contendio');
-        response.end();
-    }
-    if(request.url === '/eliminar'){
-        try {
-            await fs.unlink('./folder/ejemplo.txt');
-            response.write('Ya no queda nada...');
-            response.end();
-        } catch (error) {
-            response.write('A donde me mandaste flaco?????');
-            response.end();
-        }
-    }
+const express = require('express');
+const fs = require('fs/promises');
+const app = express();
+
+const PORT = 8080;
+
+app.use(express.json()); //Me parsea a JSON lo que me entra por body.
+
+app.get('/product', async (req, res) => {
+    const data = await fs.readFile(__dirname+'/db/db.txt', 'utf-8');
+    res.json(JSON.parse(data))
+});
+app.get('/product/:id', async (req, res) => {
+    const id = req.params.id; // query y params SIEMPRE vienen en string
+    const data = await fs.readFile(__dirname+'/db/db.txt', 'utf-8');
+    const products = JSON.parse(data);
+    //const product = products.filter(product => product.id === Number(id)); // retorna una coincidencia o undefined
+    const product = products.filter(product => {
+        // operacion
+        return product.id === Number(id)
+    }); 
+    if(product.length == 0) return res.json({message: "Product not found."});
+    res.json(product);
+});
+app.post('/product', async (req, res) => {
+    const newProduct = req.body;
+    const data = await fs.readFile(__dirname+'/db/db.txt', 'utf-8');
+    const products = JSON.parse(data);
+    // products.push({id: products.length+1, product: newProduct.product, price: newProduct.price})
+    products.push({id: products.length+1, ...newProduct}) // -> le meto adentro todo lo de newProduct
+    console.log(products);
+    const productString = JSON.stringify(products)
+    await fs.writeFile(__dirname+'/db/db.txt', productString, 'utf-8');
+    res.json({message: "Agregando el nuevo producto. "})
+});
+/*app.put();
+app.delete(); */
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
 })
-
-server.listen(8080);
-
-console.log('Servidor escuchando en puerto 8080');
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Vamos a armar un CRUD creando un servidor con Express y persistencia de datos a traves de un archivo de extensión .txt, en otras palabras, vamos a crear nuestro propio JSON-SERVER.
+
+Para llevarlo a cabo, vamos a requerir la dependencia Express y la dependencia de desarrollo nodemon.
+
+Deberemos contar con una ruta para:
+### Leer todos los items. ✔️ 
+### Leer un item por id.
+### Agregar un nuevo item.
+### Actualizar un item  referenciado por ID.
+### Eliminar un item referenciado por ID.
